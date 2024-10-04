55d772035fc2962710adfd371d38046da8e2d867
Artem Nistuley <artem.nistuley@gmail.com>
2024-10-04 12:22:39 +0300

    07_threads/03_leak

--- exercises/07_threads/03_leak/src/lib.rs
+++ exercises/07_threads/03_leak/src/lib.rs
@@ -6,7 +6,17 @@
 use std::thread;
 
 pub fn sum(v: Vec<i32>) -> i32 {
-    todo!()
+    let v: &'static mut [i32] = v.leak();
+    let (v1, v2) = v.split_at(v.len() / 2);
+
+    let handle = thread::spawn(move || {
+        v1.iter().sum::<i32>()
+    });
+    let handle2 = thread::spawn(move || {
+        v2.iter().sum::<i32>()
+    });
+
+    handle.join().unwrap() + handle2.join().unwrap()
 }
 
 #[cfg(test)]
