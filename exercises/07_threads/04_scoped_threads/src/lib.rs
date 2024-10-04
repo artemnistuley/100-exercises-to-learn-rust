e3059421039d0e5ecc6075e6e987406e5a5ecc27
Artem Nistuley <artem.nistuley@gmail.com>
2024-10-04 12:39:05 +0300

    07_threads/04_scoped_threads

--- exercises/07_threads/04_scoped_threads/src/lib.rs
+++ exercises/07_threads/04_scoped_threads/src/lib.rs
@@ -2,9 +2,40 @@
 //  and compute the sum of each half in a separate thread.
 //  Don't perform any heap allocation. Don't leak any memory.
 
+use std::thread;
+
 pub fn sum(v: Vec<i32>) -> i32 {
-    todo!()
+    let (v1, v2) = v.split_at(v.len() / 2);
+
+    let result = thread::scope(|scope| {
+        let s1 = scope.spawn(|| {
+            v1.iter().sum::<i32>()
+        });
+        let s2 = scope.spawn(|| {
+            v2.iter().sum::<i32>()
+        });
+
+        s1.join().unwrap() + s2.join().unwrap()
+    });
+
+    result
 }
+
+// let v = vec![1, 2, 3];
+// let midpoint = v.len() / 2;
+
+// std::thread::scope(|scope| {
+//     scope.spawn(|| {
+//         let first = &v[..midpoint];
+//         println!("Here's the first half of v: {first:?}");
+//     });
+//     scope.spawn(|| {
+//         let second = &v[midpoint..];
+//         println!("Here's the second half of v: {second:?}");
+//     });
+// });
+
+// println!("Here's v: {v:?}");
 
 #[cfg(test)]
 mod tests {
